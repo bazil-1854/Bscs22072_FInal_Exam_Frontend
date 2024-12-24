918d89bb546d1c9ff43087d847120a032f05ff25
bazil-1854 <bscs22072@itu.edu.pk>
2024-12-24 11:08:43 +0500

    Integrate SignUp of a User

--- src/Components/Authentication/SignUp.jsx
+++ src/Components/Authentication/SignUp.jsx
@@ -8,15 +8,15 @@ import { FaUserCircle } from 'react-icons/fa';
 import { useAuthContext } from '../../AuthProvider';
 
 const SignUpForm = () => {
-  const { showToast} = useAuthContext();
+  //const { showToast } = useAuthContext();
   const navigate = useNavigate();
   const [focusField, setFocusField] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const [formData, setFormData] = useState({
     fullName: '',
     email: '',
     password: '',
-    role: 'Guest',
   });
 
   const { email, password, fullName } = formData;
@@ -36,26 +36,27 @@ const SignUpForm = () => {
     }*/
   };
 
-  const handleRoleChange = (role) => {
+  /*const handleRoleChange = (role) => {
     setFormData((prev) => ({ ...prev, role }));
-  };
+  };*/
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const response = await axios.post(`${import.meta.env.VITE_REACT_APP_API_BASE_URL}/air-bnb/auth/register`, formData);
-      //alert('Registration successful!');
-      showToast("Registration successfull! 😇");
+      const response = await axios.post(`${import.meta.env.VITE_REACT_APP_API_BASE_URL}/task-manager/auth/register`, formData);
+      alert('Registration successful!');
+      //showToast("Registration successfull! 😇");
       navigate('/signIn');
     }
     catch (error) {
       console.error(error);
       setLoading(false);
-      //alert('Registration failed!' + error.response.data.error);
-      showToast('Registration failed! ' + error.response.data.error);
+      alert('Registration failed!' + error.response.data.error);
+      //showToast('Registration failed! ' + error.response.data.error);
     }
   };
+
   const handleFocus = (field) => {
     setFocusField(field);
   };
@@ -63,13 +64,13 @@ const SignUpForm = () => {
   const handleBlur = () => {
     setFocusField('');
   };
-  
+
   if (loading) {
     return <div className="bg-white w-full min-h-screen flex justify-center items-center">
       <div>
         <div className="animate-spin"><div className="lg:scale-[0.7] scale-[0.65] custom-loader"></div></div>
-        <p className='text-[14px] text-rose-700 font-[700]'>Registering Your Account In ...</p>
-        <p className='text-[14px] text-rose-500 font-[600]'>Please Wait</p>
+        <p className='text-[14px] text-gray-700 font-[700]'>Registering Your Account In ...</p>
+        <p className='text-[14px] text-gray-500 font-[600]'>Please Wait</p>
       </div>
     </div>;
   }
@@ -79,66 +80,33 @@ const SignUpForm = () => {
       <div className='flex flex-col w-[90%] md:w-[500px] md:scale-[1] scale-[0.9]'>
 
         <div className="scale-[1.2] flex mx-auto">
-          <img src={collaboratorLogo} alt="Connection Failed" className="w-[34px] h-[30px] mt-[4px]" />
-          <div className="text-rose-600 ml-[4px] md:text-[25px] text-[25px] font-[700]">Air<span className='text-red-400'>BnB</span></div>
+          {/*  <img src={collaboratorLogo} alt="Connection Failed" className="w-[34px] h-[30px] mt-[4px]" />
+          */}
+          <div className="text-gray-600 ml-[4px] md:text-[25px] text-[25px] font-[700]">Task-<span className='text-gray-400'>Manager</span></div>
         </div>
-        <p className='text-[15px] mb-[15px] text-center mt-[8px] text-gray-500 font-[400]'>Stay, Host, Explore. To start your adventure <span className='text-rose-700 font-[600]'>Register Now !!</span></p>
+        <p className='text-[15px] mb-[15px] text-center mt-[8px] text-gray-500 font-[400]'>Stay, Host, Explore. To start your adventure <span className='text-gray-700 font-[600]'>Register Now !!</span></p>
 
         <div className='py-[35px] px-[25px] flex flex-col bg-white rounded-xl'>
           <form onSubmit={handleSubmit} className='lg:px-[15px]'>
 
-            {/*<div className="mb-4">
-            <p className="text-sm font-medium text-gray-600 mb-2">
-              How would you like to continue?
-            </p>
-            <div className="flex items-center mb-2">
-              <input
-                type="checkbox"
-                id="guestRole"
-                name="role"
-                value="Guest"
-                checked={formData.role === 'Guest'}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              <label htmlFor="guestRole" className="text-sm text-gray-600">
-                As a Guest
-              </label>
-            </div>
-  
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                id="hostRole"
-                name="role"
-                value="Host"
-                checked={formData.role === 'Host'}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              <label htmlFor="hostRole" className="text-sm text-gray-600">
-                As a Host
-              </label>
-            </div>
-          </div>*/}
 
-            <div className="flex mb-[8px] justify-center space-x-4">
-              <label className={`flex items-center w-[50%] text-rose-600 space-x-2 p-2 border-2 rounded-lg cursor-pointer ${formData.role === 'Guest' ? 'border-rose-600 bg-rose-100' : 'border-gray-300'}`}
+            {/*<div className="flex mb-[8px] justify-center space-x-4">
+              <label className={`flex items-center w-[50%] text-gray-600 space-x-2 p-2 border-2 rounded-lg cursor-pointer ${formData.role === 'Guest' ? 'border-gray-600 bg-gray-100' : 'border-gray-300'}`}
                 onClick={() => handleRoleChange('Guest')}
               >
                 <FaUserCircle size={22} />
-                <span className='text-rose-950 text-[17px]'>Guest</span>
+                <span className='text-gray-950 text-[17px]'>Guest</span>
               </label>
-              <label className={`flex items-center w-[50%] text-rose-600 space-x-2 p-2 border-2 rounded-lg cursor-pointer ${formData.role === 'Host' ? 'border-rose-600 bg-rose-100' : 'border-gray-300'}`}
+              <label className={`flex items-center w-[50%] text-gray-600 space-x-2 p-2 border-2 rounded-lg cursor-pointer ${formData.role === 'Host' ? 'border-gray-600 bg-gray-100' : 'border-gray-300'}`}
                 onClick={() => handleRoleChange('Host')}
               >
                 <FaUserCircle size={22} />
-                <span className='text-rose-950 text-[17px]'>Host</span>
+                <span className='text-gray-950 text-[17px]'>Host</span>
               </label>
             </div>
-
+*/}
             <div className="relative mt-[25px] mb-6 flex items-center">
-              <div className="bg-rose-700 mr-2 rounded-full flex items-center justify-center w-[40px] h-[40px]">
+              <div className="bg-gray-700 mr-2 rounded-full flex items-center justify-center w-[40px] h-[40px]">
                 <CgProfile className="text-gray-50 text-[22px]" />
               </div>
 
@@ -154,14 +122,14 @@ const SignUpForm = () => {
                   onChange={handleChange}
                   onFocus={() => handleFocus('fullName')}
                   onBlur={handleBlur}
-                  className="w-full py-3 bg-transparent text-gray-700 border-b-[2px] border-rose-800  focus:outline-none"
+                  className="w-full py-3 bg-transparent text-gray-700 border-b-[2px] border-gray-800  focus:outline-none"
                   required
                 />
               </div>
             </div>
 
             <div className="relative mt-4 mb-6 flex items-center">
-              <div className="bg-rose-700 mr-2 rounded-full flex items-center justify-center w-[40px] h-[40px]">
+              <div className="bg-gray-700 mr-2 rounded-full flex items-center justify-center w-[40px] h-[40px]">
                 <AiOutlineMail className="text-gray-50 text-[22px]" />
               </div>
 
@@ -177,14 +145,14 @@ const SignUpForm = () => {
                   onChange={handleChange}
                   onFocus={() => handleFocus('email')}
                   onBlur={handleBlur}
-                  className="w-full py-3 bg-transparent text-gray-700 border-b-[2px] border-rose-800  focus:outline-none"
+                  className="w-full py-3 bg-transparent text-gray-700 border-b-[2px] border-gray-800  focus:outline-none"
                   required
                 />
               </div>
             </div>
 
             <div className="relative mt-4 mb-6 flex items-center">
-              <div className="bg-rose-700 mr-2 rounded-full flex items-center justify-center w-[40px] h-[40px]">
+              <div className="bg-gray-700 mr-2 rounded-full flex items-center justify-center w-[40px] h-[40px]">
                 <AiOutlineLock className="text-gray-50 text-[22px]" />
               </div>
               <div className="flex-1">
@@ -199,13 +167,13 @@ const SignUpForm = () => {
                   onChange={handleChange}
                   onFocus={() => handleFocus('password')}
                   onBlur={handleBlur}
-                  className="w-full py-3 bg-transparent text-gray-700 border-b-[2px] border-rose-800 focus:outline-none"
+                  className="w-full py-3 bg-transparent text-gray-700 border-b-[2px] border-gray-800 focus:outline-none"
                   required
                 />
               </div>
             </div>
 
-            <button type="submit" className="hover:text-blue-100 hover:bg-blue-950 w-full my-[15px] bg-gradient-to-r from-pink-800 via-rose-700 to-pink-800 rounded-lg text-white font-[500] py-[12px] transition duration-300">
+            <button type="submit" className="hover:text-blue-100 hover:bg-blue-950 w-full my-[15px] bg-gradient-to-r from-gray-800 via-gray-600 to-gray-800 rounded-lg text-white font-[500] py-[12px] transition duration-300">
               Sign Up
             </button>
 
@@ -217,7 +185,7 @@ const SignUpForm = () => {
             <div className='w-[47%] h-[2px] bg-[#c5c5c5]'></div>
           </div>
 
-          <p className='mx-auto cursor-pointer mt-[18px] text-gray-500 font-medium'>Already Have An Account?<span onClick={() => navigate("/signIn")} className='text-rose-700 ml-[8px] underline'>Sign In</span></p>
+          <p className='mx-auto cursor-pointer mt-[18px] text-gray-500 font-medium'>Already Have An Account?<span onClick={() => navigate("/signIn")} className='text-gray-700 ml-[8px] underline'>Sign In</span></p>
 
         </div>
 
